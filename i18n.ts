7f76aba56a716f25c39a46d969b561d976a03def
aleksey-10 <alexcare1337@gmail.com>
2021-01-16 22:36:44 +0200

    i18n update

---
+++ i18n.ts
@@ -0,0 +1,20 @@
+import NextI18Next from 'next-i18next';
+import path from 'path';
+
+const localeSubpaths = {
+  en: 'en',
+  ru: 'ru',
+  ua: 'ua',
+};
+
+const NextI18NextInstance = new NextI18Next({
+  browserLanguageDetection: false,
+  fallbackLng: 'ru',
+  defaultLanguage: 'ru',
+  otherLanguages: ['en', 'ua', 'ru'],
+  localeSubpaths,
+  localePath: path.resolve('./public/static/locales')
+});
+
+export const { appWithTranslation, useTranslation, withTranslation, Link, i18n} = NextI18NextInstance;
+export default NextI18NextInstance;
