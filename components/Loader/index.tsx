12b9a5e5727a29b41b0f1b379271044817f3fa37
aleksey-10 <alexcare1337@gmail.com>
2021-01-17 15:18:07 +0200

    loader

---
+++ components/Loader/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { useSize } from '../../hooks/size.hooks';
+
+interface Props {
+  size?: string;
+}
+
+export const Loader = ({ size = '' }) => {
+  const fontSize = useSize(size, { sm: 22, md: 36, lg: 72 });
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader} style={{ fontSize }}>
+        {/* <div className={styles.line} /> */}
+      </div>
+    </div>
+  );
+};
