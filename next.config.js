67df584750d2565201a4be05046fdecfcf2500c7
aleksey-10 <alexcare1337@gmail.com>
2021-02-06 16:56:09 +0200

    fix build

--- next.config.js
+++ next.config.js
@@ -10,6 +10,6 @@ module.exports = {
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: { localeSubpaths },
   env: {
-    API_URL: 'https://aina-gasse.vercel.app/api',
+    API_URL: 'http://localhost:3000/api',
   },
 }
